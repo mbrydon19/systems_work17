b99da0416bb4fdbefbfc02e1f0ed68ad9e50dee0
ladyt <egulsen@stuy.edu>
2018-12-14 00:03:09 -0500

    control.c done, woo

--- control.c
+++ control.c
@@ -6,7 +6,7 @@
 #include <sys/sem.h>
 #include <string.h>
 #include <fcntl.h>
-
+#include <unistd.h>
 
 union semun {
     int              val;    /* Value for SETVAL */
@@ -19,15 +19,33 @@ union semun {
 int main( int argc, char *argv[]){
   char * arg = argv[1];
   key_t key = ftok("control.c", 'R');
+  int shm = shmget(key, 256, 0644 | IPC_CREAT);
+  int sem = semget(key, 1, 0644 | IPC_CREAT);
+  
   if(!strcmp(arg, "-c")){ // creating story
-    int shm = shmget(key, 256, 0644 | IPC_CREAT | IPC_EXCL);
-    int sem = semget(key, 1, 0644 | IPC_CREAT | IPC_EXCL);
     semctl(sem, 0, SETVAL, 1); //<- ima be honest i have no idea if this is correct
     open("story.txt", O_CREAT | O_RDWR | O_TRUNC);
-    printf("Created the semaphore and shared memory.");
+    printf("Created the semaphore and shared memory.\n");
   }
+  
   if(!strcmp(arg, "-r")){ //removing story
-      
-      
+      if (semctl(sem, 0, GETVAL)) { //waiing for semahpore to be open
+        shmctl(shm, IPC_RMID, NULL);
+        semctl(sem, 0, IPC_RMID);
+        int f = open("story.txt", O_RDONLY);
+        char content[256];
+        read(f, content, 256);
+        printf("The story:\n%s\n\n", content);
+        remove("story.txt");
+        printf("Removed the semaphore, the story, and the shared memory.\n");
+      }
   }
+  
+  if(!strcmp(arg, "-v")){ //viewing story
+      int f = open("story.txt", O_RDONLY);
+      char content[256];
+      read(f, content, 256);
+      printf("The story:\n%s\n\n", content);
+  }
+  
 }
