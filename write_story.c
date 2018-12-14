c9a3d0abba6cad5430863e06a6e9d6b1fd818eac
Maia Brydon <mbrydon@stuy.edu>
2018-12-14 10:46:12 -0500

    updated write file

--- write_story.c
+++ write_story.c
@@ -1,17 +1,24 @@
 int main(){
-    key_t key = ftok("control.c", 'R');
-    int sem = semget(key, 1, IPC_EXCL);
-    if (semctl(sem, 0, GETVAL)){
-      struct sembuf * sb_down;
-      sb.sem_num = 0;
-      sb.sem_op = -1;
-      semop(sem, 1, sb_down);
+  key_t key = ftok("control.c", 'R');
+  int sem = semget(key, 1, IPC_EXCL);
+  if (semctl(sem, 0, GETVAL)){
+    //down the semaphore
+    struct sembuf * sb_down;
+    sb.sem_num = 0;
+    sb.sem_op = -1;
+    semop(sem, 1, sb_down);
+    //get and print last line
+    char * last_line[256];
+    shmat(key, last_line, 256);
     printf("Last line of the story: %s\n", );
+    //asks for next line
     printf("Enter the next line of the story!:\n");
     char * next_line[256];
     scanf("%s", &next_line);
+    //write next line to story file
     int file = open("story.txt", O_WRONLY);
     write(file, next_line, 256);
+    //put next line into shared memory
     int shm = shmget(key, 256, IPC_EXCL);
     shmat(shm, )
   }
