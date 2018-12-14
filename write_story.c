f5018f8cd997a92d53496f060cedfed84a54f35f
Maia Brydon <mbrydon@stuy.edu>
2018-12-14 10:56:20 -0500

    updated write file

--- write_story.c
+++ write_story.c
@@ -1,3 +1,13 @@
+#include <stdio.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/types.h>
+#include <sys/sem.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 int main(){
   key_t key = ftok("control.c", 'R');
   int sem = semget(key, 1, IPC_EXCL);
@@ -10,7 +20,7 @@ int main(){
     //get and print last line
     char * last_line[256];
     shmat(key, last_line, 256);
-    printf("Last line of the story: %s\n", );
+    printf("Last line of the story: %s\n", last_line);
     //asks for next line
     printf("Enter the next line of the story!:\n");
     char * next_line[256];
@@ -21,4 +31,9 @@ int main(){
     //put next line into shared memory
     int shm = shmget(key, 256, IPC_EXCL);
     shmat(shm, )
+    //up the semaphore
+    struct sembuf * sb_up;
+    sb.sem_num = 0;
+    sb.sem_op = 1;
+    semop(sem, 1, sb_up);
   }
